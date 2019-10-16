6496d2709e2965a1fdc9fe71533fa4ce292caba2
hansadong <hansadong@DESKTOP-AQUPJV9>
2019-10-16 11:17:13 +0900

    ownerPage 아이콘 변경 카페삭제 추가

---
+++ LugarFresco/src/com/kh/owner/controller/OwnerPage_myCafeDeleteServlet.java
@@ -0,0 +1,68 @@
+package com.kh.owner.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.member.model.service.MyPageService;
+import com.kh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class OwnerPage_myCafeDeleteServlet
+ */
+@WebServlet("/deleteMyCafe.op")
+public class OwnerPage_myCafeDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OwnerPage_myCafeDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		
+		if(loginUser != null) {
+			int cno = Integer.parseInt(request.getParameter("cno"));
+			
+			int result = new MyPageService().deleteMyCafe(cno);
+			
+			if(result > 0) {
+				session.setAttribute("msg", "카페 삭제가 완료되었습니다.");
+				response.sendRedirect("myCafeList.op");
+				
+			}else {
+				request.setAttribute("msg", "카페 삭제에 실패하였습니다.");
+				request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+				
+			}
+			
+		}else {
+			request.getRequestDispatcher("views/common/loginForm.jsp").forward(request, response);
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
